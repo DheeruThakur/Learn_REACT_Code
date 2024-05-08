660a191102e0a2e4077bc1cd6bddd4cf22b52da4
DheeruThakur <sdheerendra382@gmail.com>
2024-05-08 14:29:51 +0530

    hello world

---
+++ App.js
@@ -0,0 +1,43 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+        id : "head1"
+    },
+    "Heading1"
+)
+
+const heading2 = React.createElement(
+    "h2",
+    {
+        id : "head2"
+    },
+    "Heading2"
+)
+
+const heading3 = React.createElement(
+    "p",
+    {
+        id : "para"
+    },
+    "dheeru@Dheerus-MacBook-Air React_Code % git remote add origin https://github.com/abc/Learn_REACT_Code.git"
+)
+
+const heading4 = React.createElement(
+    "div",
+    {
+        id : "div1"
+    },
+    [heading3]
+)
+
+const container = React.createElement(
+    "div",
+    {
+        id : "container"
+    },
+    [heading1 , heading2 , heading4]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container)
+
